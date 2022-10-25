70e792334f56c741fab24acc9e5198c6b5fa95d5
tanvir <tanvirhridoy1011@gmail.com>
2022-10-26 00:19:36 +0600

    set up cors

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express=require('express')
 const app=express();
+const cors=require('cors');
 const port=process.env.PORT||5000;
+
+
+app.use(cors());
+
+const categories=require('./data/courses.json');
+
+
 app.get('/',(req,res)=>{
     res.send('tn learning is runnig');
 });
+app.get('/courses',(req,res)=>{
+    res.send(categories)
+})
 app.listen(port,()=>{
     console.log('tn-learning Server running at port ',port)
 })
